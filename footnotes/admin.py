312001ed6a026833324c37f792430638c3817b86
Jason Heppler <jason.heppler@gmail.com>
2023-03-30 15:29:51 -0500

    Added app for handling footnotes

---
+++ footnotes/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import Footnote
+
+admin.site.register(Footnote)
---
+++ footnotes/models.py
@@ -0,0 +1,61 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.db.models.functions import NullIf
+from multiselectfield import MultiSelectField
+
+from common.models import TrackChangesModel
+
+
+# Create your models here.
+class Footnote(TrackChangesModel):
+    """a footnote that links from a denig.models.Document to a class:Fragment"""
+
+    source = models.ForeignKey(
+        "denig.Document",
+        on_delete=models.CASCADE,
+    )
+    location = models.CharField(
+        max_length=255,
+        blank=True,
+        help_text="Location of the footnote in the source document (e.g., page number or page range).",
+    )
+
+    FOOTNOTE_TYPE = (
+        ("O", "Original"),
+        ("A", "Annotation"),
+    )
+
+    footnote_type = models.CharField(
+        choices=FOOTNOTE_TYPE,
+        blank=True,
+        null=True,
+        max_length=1,
+        help_text="Footnote type",
+    )
+    content = models.TextField(
+        blank=True,
+        null=True,
+    )
+    notes = models.TextField(
+        blank=True,
+    )
+
+    class Meta:
+        ordering = [
+            "source",
+            # sort footnote with empty locations after footnote with location
+            # (sort digital editions after other footnotes for same source)
+            NullIf("location", models.Value("")).asc(nulls_last=True),
+        ]
+        constraints = [
+            # only allow one digital edition per source for a document
+            models.UniqueConstraint(
+                fields=("source", "footnote_type"),
+                name="one_fragment_per_page",
+                condition=models.Q(footnote_type__contains="O"),  # X = DIGITAL_EDITION
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.source} {self.location}"

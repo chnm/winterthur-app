ba977e76467bf46210bbe907f87e67de1c3b59da
Jason Heppler <jason.heppler@gmail.com>
2023-03-31 09:19:23 -0500

    Adding custom user

---
+++ authuser/views.py
@@ -0,0 +1,34 @@
+from django.conf import settings
+from django.contrib.auth import login as login_user
+from django.contrib.auth import logout as logout_user
+from django.http import HttpResponseForbidden
+from django.shortcuts import redirect, render
+
+from .forms import SignUpForm
+from .models import User
+
+
+def signup(request):
+    if not getattr(settings, "AUTH_USER_ALLOW_SIGNUP", False):
+        return HttpResponseForbidden("Forbidden")
+
+    form = SignUpForm()
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                email=form.cleaned_data["email"],
+            )
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+            login_user(request, user)
+            return redirect("/")
+
+    return render(
+        request,
+        "registration/signup.html",
+        {
+            "form": form,
+        },
+    )

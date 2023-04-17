80471bd31e1e6ca528f1b5ca82fc432c9fb102d9
Jason Heppler <jason.heppler@gmail.com>
2023-04-17 10:24:12 -0500

    Add Fragments in Document admin view

--- denig/admin.py
+++ denig/admin.py
@@ -1,14 +1,17 @@
+from django import forms
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.forms.widgets import HiddenInput, Textarea, TextInput
+from django.urls import reverse
+from django.utils.html import format_html
+
+from footnotes.models import Footnote
 
 from .models import Collection, Document, Fragment, Language
 
 admin.site.register(Collection)
 admin.site.register(Language)
-admin.site.register(Document)
-
-## Add to the Fragment view a way to see:
-## 1. the document that the fragment is from
-## 2. the translation of the fragment
 
 
 class FragmentAdmin(admin.ModelAdmin):
@@ -18,3 +21,106 @@ class FragmentAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Fragment, FragmentAdmin)
+
+
+class DocumentForm(forms.ModelForm):
+    class Meta:
+        model = Document
+        exclude = ()
+        widgets = {
+            "language_note": Textarea(attrs={"rows": 1}),
+            "needs_review": Textarea(attrs={"rows": 3}),
+            "notes": Textarea(attrs={"rows": 3}),
+            "image_order_override": HiddenInput(),
+        }
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    form = DocumentForm
+    list_display = (
+        "docside",
+        "page",
+        "description",
+        "doctype",
+        "all_tags",
+        "last_modified",
+        "id",
+    )
+    readonly_fields = (
+        "created",
+        "last_modified",
+        "admin_thumbnails",
+        "id",
+    )
+    search_fields = (
+        "fragments",
+        "tags__name",
+        "description",
+        "notes",
+        "id",
+    )
+
+
+class FragmentInline(admin.TabularInline):
+    model = Fragment
+    extra = 0
+    verbose_name = "Fragment"
+    verbose_name_plural = "Fragments"
+    fields = (
+        "line_number",
+        "transcription",
+        "languages",
+        "notes",
+        "admin_thumbnails",
+    )
+    readonly_fields = ("admin_thumbnails",)
+    formfield_overrides = {
+        models.ManyToManyField: {"widget": forms.CheckboxSelectMultiple},
+    }
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.order_by("line_number")
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "document":
+            kwargs["initial"] = request.resolver_match.kwargs["object_id"]
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def admin_thumbnails(self, obj):
+        return format_html("<img src='{}' />", obj.image.url)
+
+    admin_thumbnails.short_description = "Image"
+
+
+DocumentAdmin.inlines = [FragmentInline]
+
+admin.site.register(Document, DocumentAdmin)
+
+
+class FootnoteInline(admin.TabularInline):
+    """Add footnotes within the Document form."""
+
+    model = Footnote
+    extra = 0
+    verbose_name = "Footnote"
+    verbose_name_plural = "Footnotes"
+    fields = ("content", "source", "id")
+    readonly_fields = ("source", "id")
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.order_by("id")
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "document":
+            kwargs["initial"] = request.resolver_match.kwargs["object_id"]
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def footnote(self, obj):
+        return format_html("<a href='{}'>{}</a>", obj.get_absolute_url(), obj)
+
+    footnote.short_description = "Footnote"
+
+
+FragmentAdmin.inlines = [FootnoteInline]

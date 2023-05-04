c25d5092bb9d7f877b1ec61e75fb20612e5c9cb7
Jason Heppler <jason.heppler@gmail.com>
2023-05-04 15:18:45 -0500

    Add static media URL to settings

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -6,4 +8,4 @@ urlpatterns = [
     path("accounts/", include("authuser.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
     path("", include("denig.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

04aa54692eb7a626ddbff07496aeaeb592f112b0
Jason Heppler <jason.heppler@gmail.com>
2023-06-20 13:49:42 -0500

    Update footnote display in admin list view

--- footnotes/admin.py
+++ footnotes/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from denig.admin import FragmentAdmin
+
 from .models import Footnote
 
 
@@ -7,4 +9,11 @@ class FootnoteAdmin(admin.ModelAdmin):
     list_display = ("source", "footnote_type", "last_modified")
 
 
+class FootnoteInline(admin.TabularInline):
+    model = Footnote
+    fields = ("content", "footnote_type", "notes")
+
+
 admin.site.register(Footnote, FootnoteAdmin)
+
+FragmentAdmin.inlines = [FootnoteInline]

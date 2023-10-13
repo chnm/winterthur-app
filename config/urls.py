5608cf61e45e2f0002586bd0e518976ce6847ac8
Jason Heppler <jason.heppler@gmail.com>
2023-10-13 09:37:14 -0500

    Fixed load order for djdt

--- config/urls.py
+++ config/urls.py
@@ -5,12 +5,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("denig.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("authuser.urls")),
-    path("__reload__/", include("django_browser_reload.urls")),
-    path("", include("denig.urls")),
     path("prose/", include("prose.urls")),
+    path("__reload__/", include("django_browser_reload.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]

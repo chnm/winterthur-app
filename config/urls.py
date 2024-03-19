c5cc47f5da3023723e6270149e60b129013547c3
Jason Heppler <jason.heppler@gmail.com>
2024-03-19 09:00:07 -0500

    Turning off browser_reload

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("authuser.urls")),
     path("prose/", include("prose.urls")),
-    path("__reload__/", include("django_browser_reload.urls")),
+    # path("__reload__/", include("django_browser_reload.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

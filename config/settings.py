192bcda8731a36b7f079d4c4516dfb13705e61ea
Jason Heppler <jason.heppler@gmail.com>
2024-05-23 09:54:27 -0500

    chore: Add media configuration for user-contributed files
    The missing MEDIA_ROOT and MEDIA_URL settings were added to the settings.py file, fixing a problem where any directory and its files could be accessed by anyone with the URL.

--- config/settings.py
+++ config/settings.py
@@ -176,6 +176,8 @@ TAGGIT_STRING_FROM_TAGS = "taggit_selectize.utils.join_tags"
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)

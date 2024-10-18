08ccc2f70c25e2150fc27355f08bb63c75a67679
Jason Heppler <jason.heppler@gmail.com>
2024-10-18 10:28:00 -0500

    refactor: Update ForensicPage model and add DocumentChooserPanel and GalleryImage

--- essays/models.py
+++ essays/models.py
@@ -5,10 +5,62 @@ from django.utils.text import slugify
 from modelcluster.fields import ParentalKey
 from prose.models import Document
 from taggit_selectize.managers import TaggableManager
-from wagtail.admin.panels import FieldPanel
-from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel, InlinePanel, PageChooserPanel
+from wagtail.fields import RichTextField, StreamField
+from wagtail.images.blocks import ImageChooserBlock
 from wagtail.models import Orderable, Page
 from wagtail.search import index
+
+from denig.models import Document
+
+
+class OrderedDocumentChooserPanel(PageChooserPanel):
+    def get_form_class(self):
+        form_class = super().get_form_class()
+        form_class.base_fields[self.field_name].queryset = Document.objects.order_by(
+            "document_id"
+        )
+        return form_class
+
+
+class GalleryImage(Orderable):
+    page = ParentalKey("ForensicPage", related_name="gallery_images")
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        FieldPanel("image"),
+        FieldPanel("caption"),
+    ]
+
+
+class ForensicPage(Page):
+    body = RichTextField(blank=True)
+    preview_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        help_text="If no preview image is provided, the first image in the gallery will be used.",
+    )
+    link_to_manuscript_page = models.URLField(blank=True)
+    related_manuscript_page = models.ForeignKey(
+        Document, null=True, blank=True, on_delete=models.SET_NULL, related_name="+"
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+        FieldPanel("preview_image"),
+        OrderedDocumentChooserPanel("related_manuscript_page"),
+        InlinePanel("gallery_images", label="Gallery images"),
+    ]
 
 
 class EssayIndexPage(Page):
